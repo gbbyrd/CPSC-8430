62d9533756510094ae84b6680148e516cc23997c
gbbyrd <graysonbyrd4@gmail.com>
2023-03-29 23:55:04 -0400

    Added more evaluation scenarios and updated spoken squad dataset

--- hw3/dataset.py
+++ hw3/dataset.py
@@ -14,24 +14,28 @@ class SpokenSquadDataset(Dataset):
         
         It then creates encodings using the BertTokenizerFast tokenizer
         """
-        if train:
+        
+        self.train = train
+        
+        if self.train:
             self.data_path = 'data/spoken_train-v1.1.json'
         else:
             self.data_path = 'data/spoken_test-v1.1.json'
-            
+        
         self.max_length = max_length
         self.stride = stride
         self.model_checkpoint = model_checkpoint
         self.tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
         
         # Sync the context, question, and answer data
-        contexts, questions, answers = self.read_data()
-        self.examples = {'context': contexts, 'question': questions, 'answer': answers}
+        contexts, questions, answers, ids = self.read_data()
+        self.examples = {'context': contexts, 'question': questions, 'answer': answers, 'id': ids}
         
         self.encodings = self.preprocess_examples()
         
     def __getitem__(self, idx):
-        return {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        return {key: val[idx] for key, val in self.encodings.items()}
+        
             
     def __len__(self):
         return len(self.encodings['input_ids'])
@@ -44,6 +48,7 @@ class SpokenSquadDataset(Dataset):
             contexts = []
             questions = []
             answers = []
+            ids = []
             
             for title in data:
                 for paragraph in title['paragraphs']:
@@ -52,15 +57,17 @@ class SpokenSquadDataset(Dataset):
                         question = qas['question']
                         answer_text = qas['answers'][0]['text']
                         answer_start = qas['answers'][0]['answer_start']
-                        
+                        id = qas['id']
                         # add an 'answer_end' to the answer
                         contexts.append(context)
                         questions.append(question)
                         answers.append({'text': answer_text, 'answer_start': answer_start})
+                        ids.append(id)
                         
-            return contexts, questions, answers
+            return contexts, questions, answers, ids
         
     def preprocess_examples(self):
+        
         questions = [q.strip() for q in self.examples["question"]]
         inputs = self.tokenizer(
             questions,
@@ -72,49 +79,70 @@ class SpokenSquadDataset(Dataset):
             return_offsets_mapping=True,
             padding="max_length",
         )
-        
-        offset_mapping = inputs.pop("offset_mapping")
-        sample_map = inputs.pop("overflow_to_sample_mapping")
-        answers = self.examples["answer"]
-        start_positions = []
-        end_positions = []
-        
-        for i, offset in enumerate(offset_mapping):
-            sample_idx = sample_map[i]
-            answer = answers[sample_idx]
-            start_char = answer["answer_start"]
-            end_char = answer["answer_start"] + len(answer["text"])
-            sequence_ids = inputs.sequence_ids(i)
+            
+        if self.train == True:
+            offset_mapping = inputs.pop("offset_mapping")
+            sample_map = inputs.pop("overflow_to_sample_mapping")
+            answers = self.examples["answer"]
+            start_positions = []
+            end_positions = []
+            
+            for i, offset in enumerate(offset_mapping):
+                sample_idx = sample_map[i]
+                answer = answers[sample_idx]
+                start_char = answer["answer_start"]
+                end_char = answer["answer_start"] + len(answer["text"])
+                sequence_ids = inputs.sequence_ids(i)
 
-            print()
-            # Find the start and end of the context
-            idx = 0
-            while sequence_ids[idx] != 1:
-                idx += 1
-            context_start = idx
-            while sequence_ids[idx] == 1:
-                idx += 1
-            context_end = idx - 1
-
-            # If the answer is not fully inside the context, label is (0, 0)
-            if offset[context_start][0] > start_char or offset[context_end][1] < end_char:
-                start_positions.append(0)
-                end_positions.append(0)
-            else:
-                # Otherwise it's the start and end token positions
-                idx = context_start
-                while idx <= context_end and offset[idx][0] <= start_char:
+                print()
+                # Find the start and end of the context
+                idx = 0
+                while sequence_ids[idx] != 1:
                     idx += 1
-                start_positions.append(idx - 1)
+                context_start = idx
+                while sequence_ids[idx] == 1:
+                    idx += 1
+                context_end = idx - 1
 
-                idx = context_end
-                while idx >= context_start and offset[idx][1] >= end_char:
-                    idx -= 1
-                end_positions.append(idx + 1)
-        
-        inputs["start_positions"] = start_positions
-        inputs["end_positions"] = end_positions
-        return inputs    
+                # If the answer is not fully inside the context, label is (0, 0)
+                if offset[context_start][0] > start_char or offset[context_end][1] < end_char:
+                    start_positions.append(0)
+                    end_positions.append(0)
+                else:
+                    # Otherwise it's the start and end token positions
+                    idx = context_start
+                    while idx <= context_end and offset[idx][0] <= start_char:
+                        idx += 1
+                    start_positions.append(idx - 1)
+
+                    idx = context_end
+                    while idx >= context_start and offset[idx][1] >= end_char:
+                        idx -= 1
+                    end_positions.append(idx + 1)
+            
+            inputs["start_positions"] = start_positions
+            inputs["end_positions"] = end_positions
+            return inputs 
+           
+        else:
+            sample_map = inputs.pop("overflow_to_sample_mapping")
+            example_ids = []
+
+            for i in range(len(inputs["input_ids"])):
+                sample_idx = sample_map[i]
+                example_ids.append(self.examples["id"][sample_idx])
+
+                sequence_ids = inputs.sequence_ids(i)
+                offset = inputs["offset_mapping"][i]
+                inputs["offset_mapping"][i] = [
+                    o if sequence_ids[k] == 1 else None for k, o in enumerate(offset)
+                ]
+
+        inputs["example_id"] = example_ids
+        return inputs
+    
+    def get_unprocessed_data(self):
+        return self.examples
         
 # if __name__=='__main__':
 #     dataset = SpokenSquadDataset()
