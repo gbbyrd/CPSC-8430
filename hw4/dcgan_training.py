1f51b689a5182bfd5e5cb6053211a0ee650f34dd
gbbyrd <graysonbyrd4@gmail.com>
2023-04-14 17:29:30 -0400

    Modified dcgan training script

--- hw4/dcgan_training.py
+++ hw4/dcgan_training.py
@@ -26,7 +26,6 @@ dataset = dset.CIFAR10(root="./data", download=True,
                                transforms.ToTensor(),
                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
-nc=3
 
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=128,
                                          shuffle=True, num_workers=2)
@@ -34,16 +33,7 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=128,
 #checking the availability of cuda devices
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# number of gpu's available
-ngpu = 1
-# input noise dimension
-nz = 100
-# number of generator filters
-ngf = 64
-#number of discriminator filters
-ndf = 64
-
-# custom weights initialization called on netG and netD
+# custom weights initialization called on generator and discriminator
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
@@ -51,6 +41,8 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
+        
+noise_dim = 100
 
 # class Generator(nn.Module):
 #     def __init__(self, ngpu):
@@ -58,23 +50,23 @@ def weights_init(m):
 #         self.ngpu = ngpu
 #         self.main = nn.Sequential(
 #             # input is Z, going into a convolution
-#             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
-#             nn.BatchNorm2d(ngf * 8),
+#             nn.ConvTranspose2d(noise_dim, num_generator_filters * 8, 4, 1, 0, bias=False),
+#             nn.BatchNorm2d(num_generator_filters * 8),
 #             nn.ReLU(True),
-#             # state size. (ngf*8) x 4 x 4
-#             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-#             nn.BatchNorm2d(ngf * 4),
+#             # state size. (num_generator_filters*8) x 4 x 4
+#             nn.ConvTranspose2d(num_generator_filters * 8, num_generator_filters * 4, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(num_generator_filters * 4),
 #             nn.ReLU(True),
-#             # state size. (ngf*4) x 8 x 8
-#             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-#             nn.BatchNorm2d(ngf * 2),
+#             # state size. (num_generator_filters*4) x 8 x 8
+#             nn.ConvTranspose2d(num_generator_filters * 4, num_generator_filters * 2, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(num_generator_filters * 2),
 #             nn.ReLU(True),
-#             # state size. (ngf*2) x 16 x 16
-#             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
-#             nn.BatchNorm2d(ngf),
+#             # state size. (num_generator_filters*2) x 16 x 16
+#             nn.ConvTranspose2d(num_generator_filters * 2, num_generator_filters, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(num_generator_filters),
 #             nn.ReLU(True),
-#             # state size. (ngf) x 32 x 32
-#             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+#             # state size. (num_generator_filters) x 32 x 32
+#             nn.ConvTranspose2d(num_generator_filters, nc, 4, 2, 1, bias=False),
 #             nn.Tanh()
 #             # state size. (nc) x 64 x 64
 #         )
@@ -128,11 +120,11 @@ class Generator(nn.Module):
         
         return x
 
-netG = Generator(ngpu).to(device)
-netG.apply(weights_init)
+generator = Generator().to(device)
+generator.apply(weights_init)
 #load weights to test the model
-#netG.load_state_dict(torch.load('weights/netG_epoch_24.pth'))
-print(netG)
+#generator.load_state_dict(torch.load('weights/netG_epoch_24.pth'))
+print(generator)
 
 # class Discriminator(nn.Module):
 #     def __init__(self, ngpu):
@@ -209,73 +201,67 @@ class Discriminator(nn.Module):
         
         return x.view(-1, 1)
 
-netD = Discriminator(ngpu).to(device)
-netD.apply(weights_init)
-#load weights to test the model 
-#netD.load_state_dict(torch.load('weights/netD_epoch_24.pth'))
-print(netD)
+discriminator = Discriminator().to(device)
+discriminator.apply(weights_init)
+print(discriminator)
 
+# BCE is used for GAN training
 criterion = nn.BCELoss()
 
 # setup optimizer
-optimizerD = optim.Adam(netD.parameters(), lr=0.0002, betas=(0.5, 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizer_d = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizer_g = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
-fixed_noise = torch.randn(128, nz, 1, 1, device=device)
+fixed_noise = torch.randn(128, noise_dim, 1, 1, device=device)
 real_label = 1
 fake_label = 0
 
-niter = 25
+epochs = 50
 g_loss = []
 d_loss = []
 
-for epoch in range(niter):
+for epoch in range(epochs):
     for i, data in enumerate(dataloader, 0):
-        ############################
-        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-        ###########################
+
         # train with real
-        netD.zero_grad()
+        discriminator.zero_grad()
         real_cpu = data[0].to(device)
         batch_size = real_cpu.size(0)
         label = torch.full((batch_size,), real_label, device=device)
 
-        output = netD(real_cpu)
+        output = discriminator(real_cpu)
         errD_real = criterion(output, label)
         errD_real.backward()
         D_x = output.mean().item()
 
         # train with fake
-        noise = torch.randn(batch_size, nz, 1, 1, device=device)
-        fake = netG(noise)
+        noise = torch.randn(batch_size, noise_dim, 1, 1, device=device)
+        fake = generator(noise)
         label.fill_(fake_label)
-        output = netD(fake.detach())
+        output = discriminator(fake.detach())
         errD_fake = criterion(output, label)
         errD_fake.backward()
         D_G_z1 = output.mean().item()
         errD = errD_real + errD_fake
-        optimizerD.step()
+        optimizer_d.step()
 
-        ############################
-        # (2) Update G network: maximize log(D(G(z)))
-        ###########################
-        netG.zero_grad()
-        label.fill_(real_label)  # fake labels are real for generator cost
-        output = netD(fake)
+        generator.zero_grad()
+        label.fill_(real_label)
+        output = discriminator(fake)
         errG = criterion(output, label)
         errG.backward()
         D_G_z2 = output.mean().item()
-        optimizerG.step()
+        optimizer_g.step()
 
-        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f' % (epoch, niter, i, len(dataloader), errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f' % (epoch, epochs, i, len(dataloader), errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
         
         #save the output
         if i % 100 == 0:
             print('saving the output')
             vutils.save_image(real_cpu,'output/real_samples.png',normalize=True)
-            fake = netG(fixed_noise)
+            fake = generator(fixed_noise)
             vutils.save_image(fake.detach(),'output/fake_samples_epoch_%03d.png' % (epoch),normalize=True)
     
     # Check pointing for every epoch
-    torch.save(netG.state_dict(), 'weights/netG_epoch_%d.pth' % (epoch))
-    torch.save(netD.state_dict(), 'weights/netD_epoch_%d.pth' % (epoch))
+    torch.save(generator.state_dict(), 'weights/netG_epoch_%d.pth' % (epoch))
+    torch.save(discriminator.state_dict(), 'weights/netD_epoch_%d.pth' % (epoch))
